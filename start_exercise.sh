5da2e711b42807f8cf0c78629a14f18de005197f
Shreyas Gokhale <shreyas6gokhale@gmail.com>
2020-06-27 01:04:53 +0200

    Docker: Working exercise, can drive the robot

--- start_exercise.sh
+++ start_exercise.sh
@@ -2,6 +2,6 @@ source /opt/ros/noetic/setup.bash
 
 sleep 2s
 
-cd /exercises/amazon_warehouse/launch && roslaunch amazonrobot_1_warehouse.launch &
+cd /exercises/amazon_warehouse/launch && roslaunch amazonrobot_1_warehouse.launch map_file:=/exercises/amazon_warehouse/launch/maps/map.yaml &
 
 cd /exercises/amazon_warehouse/ && python3 amazonWarehouse.py amazonMap.conf amazonConf.yml
